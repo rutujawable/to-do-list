d6a364e10f5bed695a8f7c117e89522150c8949b
rutujawable <rutujawable0@gmail.com>
2024-07-01 23:15:49 +0530

    to-do-list added

---
+++ src/components/ToDoCard/ToDoCard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./ToDoCard.css"
+import ImgDel from "./delete.png"
+
+
+function ToDoCard({index,task,category,deleteItem}) {
+
+  const CATEGORY_EMOJI_MAP = {
+    Learning:"📚",
+    work:"🧑‍🏭",
+    Shopping:"🛒",
+    health:"🧑‍⚕️",
+    others:"🎑"
+  
+  }
+  const CATEGORY_COLOURS ={
+    Learning:"blue",
+    Work:"Purple",
+    Shopping:"green",
+    Health:"red",
+    Others:"gray",
+    Personal:"pink"
+  
+  }
+  return (
+    <div className='todo-card'>
+       <img src={ImgDel }
+        alt="Delete" className='delete-icon' 
+        onClick={()=>{
+        deleteItem(index)
+       }}/>
+     {task}
+      <span className='category' style={{
+        backgroundColor: CATEGORY_COLOURS[category]
+      }}>
+     {CATEGORY_EMOJI_MAP[category] } {category}
+      </span>
+    
+    </div>
+  )
+}
+
+export default ToDoCard
